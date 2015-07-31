7cac595cdf1d3769f7ba21f0293163e491413935
eduardogalindez <eduardo.galindez@live.com>
2015-07-31 09:54:20 -0400

    Added comments to the code

--- RegularExp.R
+++ RegularExp.R
@@ -1,3 +1,8 @@
+#This R program will take 2 arrays(one with regular expressions and the other with a replacement for that regular expression), and it send these two arrays
+#to a function that iterates all the regular expressions through a vector and replaces the ones that match with their respective replacement.
+#In this program, I applied it to mortality codes and recoded them so that they where easier to recognize.
+
+#Array with all the regular expressions
 RExp <-c('A1[6-9].+','A5[0-3].?','B2[0-4]','C16.?','C1[8-9].?|C2[0-1].?','C25.?',
          'C3[3-4].?','C50.?','C5[3-6].?','C61.?','C6[4-8].?','C8[2-5].?','C9[1-5].?',
        'C0[0-9].?|C1[0-5].?|C17.?|C2[2-4].?|C2[6-9].?|C3[0-2].?|C3[7-9].?|C4[0-9].?|C5[1-2].?|C5[7-9].?|C60.?|C6[2-3].?|C69.?|C7[0-9].?|C8[0-1].?|C88.?|C90.?|C9[6-7].?'
@@ -12,6 +17,7 @@ RExp <-c('A1[6-9].+','A5[0-3].?','B2[0-4]','C16.?','C1[8-9].?|C2[0-1].?','C25.?'
        ,'U03.?|X[6-7][0-9].?|X8[0-4].?|Y87.0.?','U0[1-2].?|X8[5-9].?|X9[0-9].?|Y0[0-9].?|Y871.?',
        'Y[1-2][0-9].?|Y3[0-6].?|Y872.?|Y89.?','[S-T][0-9][0-9].?'
        )
+#Replacements
 Recode <- c('001','002','003','005','006','007',
           '008','009','010','011','012','013',
           '014','015','004','016','017','020',
@@ -19,16 +25,22 @@ Recode <- c('001','002','003','005','006','007',
           '018','027','028','029','030','031',
           '032','033','034','035','036','037',
           '038','039','040','041','042','039')
+#This part can be omited since it is related to the work I was doing
 load("~/Desktop/mortality/2009morterPR.RData")
+#Make a vector off of the table I had and make a new shorter one after that.
 econd <- mortdataPR$econd1
 econdnew<- econd[1:200]
 
+#Function that takes two arrays and a vector as parameters, the first array is made of regular expressions
+#the second array is made of the replacements for the regular expressions and the vector is where the changes will be made.
+#It works by calling a for function through the first array and calling the sub function as many times as there are elements on the array's.
+#It returns a vector with the changes that where made.
 CodeToRecode <- function(RegularExp,CodeReplacement , CodeList){
 for(i in 1:length(RegularExp))
       CodeList<-sub(RegularExp[i],CodeReplacement[i],CodeList, perl = TRUE)
 return (CodeList)
 }
-
+#Called the vector, then the function and called the vector again to see the changes.
 econdnew
 econdnew <- CodeToRecode(RExp, Recode, econdnew)
 econdnew
